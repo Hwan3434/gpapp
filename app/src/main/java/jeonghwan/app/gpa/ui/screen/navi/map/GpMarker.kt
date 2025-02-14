6f82542fe33a57eedf2e488ccc3afdabedee48bb
이정환/기술지원실/II <cookiegon3434@injeinc.co.kr>
2025-02-14 11:06:25 +0900

    compose 구조 개선

--- app/src/main/java/jeonghwan/app/gpa/ui/screen/navi/map/GpMarker.kt
+++ app/src/main/java/jeonghwan/app/gpa/ui/screen/navi/map/GpMarker.kt
@@ -9,17 +9,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.naver.maps.map.compose.MarkerComposable
 import com.naver.maps.map.compose.MarkerState
+import com.naver.maps.map.overlay.Marker
+import jeonghwan.app.entity.GpGeoPoint
+import jeonghwan.app.entity.PersonEntity
+import jeonghwan.app.entity.TombEntity
 
 @OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun GpWindow(
-    mapViewModel: MapViewModel,
-    tempTomb: TempTomb
+    tempTomb: TempTomb,
+    onClick: (Marker) -> Boolean = { false }
 ) {
     val p = tempTomb.person.first()
     val caption = if(p.gender) {
@@ -33,10 +38,7 @@ fun GpWindow(
         keys = keysArray,
         state = MarkerState(LatLng(tempTomb.tomb.location.latitude + 0.0001, tempTomb.tomb.location.longitude)),
         captionText = caption,
-        onClick = {
-            mapViewModel.toggleWindowVisible(tempTomb.tomb.key)
-            false
-        },
+        onClick = onClick,
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -53,5 +55,44 @@ fun GpWindow(
                 tint = Color.Magenta,
             )
         }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GpWindowPreview() {
+    // 더미 데이터 생성
+    val dummyPerson = PersonEntity(
+        personKey = 1,
+        name = "John Doe",
+        family = "Doe Family",
+        clan = "Doe Clan",
+        alive = true,
+        etc = "",
+        spouse = 0,
+        generator = 1,
+        gender = true,
+        tombKey = 1
+    )
+
+    val dummyTomb = TempTomb(
+        tomb = TombEntity(
+            key = 1,
+            name = "Doe Tomb",
+            location = GpGeoPoint(
+                latitude = 37.5666103,
+                longitude = 126.9783882
+            )
+        ),
+        person = listOf(dummyPerson),
+        isWindowVisible = true
+    )
+
+    // GpWindow 호출
+    GpWindow(
+        tempTomb = dummyTomb,
+    ) {
+        false
     }
 }
--- app/src/main/java/jeonghwan/app/gpa/ui/screen/navi/map/MapScreen.kt
+++ app/src/main/java/jeonghwan/app/gpa/ui/screen/navi/map/MapScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -25,6 +26,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.naver.maps.geometry.LatLng
@@ -36,10 +38,14 @@ import com.naver.maps.map.compose.MapType
 import com.naver.maps.map.compose.MapUiSettings
 import com.naver.maps.map.compose.NaverMap
 import com.naver.maps.map.compose.rememberCameraPositionState
+import com.naver.maps.map.overlay.Marker
+import jeonghwan.app.entity.GpGeoPoint
+import jeonghwan.app.entity.PersonEntity
+import jeonghwan.app.entity.TombEntity
 
 
-fun MapType.changedNextMapType() : MapType {
-    return when(this){
+fun MapType.changedNextMapType(): MapType {
+    return when (this) {
         MapType.Basic -> MapType.Hybrid
         MapType.Hybrid -> MapType.Navi
         MapType.Navi -> MapType.Terrain
@@ -49,8 +55,9 @@ fun MapType.changedNextMapType() : MapType {
         MapType.None -> MapType.Basic
     }
 }
+
 fun MapType.toKorean(): String {
-    return when(this){
+    return when (this) {
         MapType.Basic -> "일반"
         MapType.Hybrid -> "위성(겹쳐보기)"
         MapType.Navi -> "네비게이션"
@@ -65,7 +72,7 @@ private val naviMapSaver = Saver<MapType, String>(
     save = { it.value.name },
     restore = { route ->
         route.let {
-            when(it){
+            when (it) {
                 MapType.Basic.value.name -> MapType.Basic
                 MapType.Hybrid.value.name -> MapType.Hybrid
                 MapType.Navi.value.name -> MapType.Navi
@@ -79,16 +86,32 @@ private val naviMapSaver = Saver<MapType, String>(
     }
 )
 
-@OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun MapScreen(
     modifier: Modifier = Modifier,
     viewModel: MapViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.uiState.collectAsState()
+    MapItemScreen(
+        modifier = modifier,
+        uiState = uiState,
+    ) { tomb ->
+        viewModel.toggleWindowVisible(tomb.key)
+        false
+    }
+}
+
+@OptIn(ExperimentalNaverMapApi::class)
+@Composable
+fun MapItemScreen(
+    modifier: Modifier,
+    uiState: MapUiState,
+    onClick: (TombEntity) -> Boolean = { false }
+){
+
     var mapState by rememberSaveable(stateSaver = naviMapSaver) { mutableStateOf(MapType.Satellite) }
 
-    if(uiState.isLoading){
+    if (uiState.isLoading) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
@@ -101,7 +124,7 @@ fun MapScreen(
 
     val cameraPositionState = rememberCameraPositionState(
         init = {
-            position = CameraPosition(LatLng(36.615743, 128.352462), 16.0,)
+            position = CameraPosition(LatLng(36.615743, 128.352462), 16.0)
         }
     )
 
@@ -113,7 +136,6 @@ fun MapScreen(
             modifier = modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState,
             properties = MapProperties(
-                mapState,
                 locationTrackingMode = LocationTrackingMode.Follow,
             ),
             uiSettings = MapUiSettings(
@@ -124,8 +146,10 @@ fun MapScreen(
         ) {
             uiState.tempTomb.map { tempTomb ->
                 GpWindow(
-                    mapViewModel = viewModel,
-                    tempTomb = tempTomb
+                    tempTomb = tempTomb,
+                    onClick = { _ ->
+                        onClick(tempTomb.tomb)
+                    }
                 )
             }
         }
@@ -158,4 +182,48 @@ fun MapScreen(
             }
         }
     }
+}
+
+@OptIn(ExperimentalNaverMapApi::class)
+@Preview(showBackground = true)
+@Composable
+fun MapItemScreenPreview() {
+    // 더미 UI 상태 생성
+    val dummyUiState = MapUiState(
+        isLoading = false,
+        tempTomb = listOf(
+            TempTomb(
+                tomb = TombEntity(
+                    key = 1,
+                    name = "Doe Tomb",
+                    location = GpGeoPoint(latitude = 36.615743, longitude = 128.352462)
+                ),
+                person = listOf(
+                    PersonEntity(
+                        personKey = 1,
+                        name = "John Doe",
+                        family = "Doe Family",
+                        clan = "Doe Clan",
+                        alive = true,
+                        etc = "",
+                        spouse = 0,
+                        generator = 1,
+                        gender = true,
+                        tombKey = 1
+                    )
+                ),
+                isWindowVisible = true
+            )
+        )
+    )
+
+    // MapItemScreen 호출
+    MapItemScreen(
+        modifier = Modifier.fillMaxSize(),
+        uiState = dummyUiState,
+        onClick = { tombEntity ->
+            // 클릭 시 동작
+            true
+        }
+    )
 }
