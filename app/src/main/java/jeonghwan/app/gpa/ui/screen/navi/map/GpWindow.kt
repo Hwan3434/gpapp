15ce78842622ff2cd3a1cc1ef88fda0383832753
jeonghwan <cookiegon3434@gmail.com>
2025-03-04 22:32:43 +0900

    네이버 맵 윈도우 최대 가로길이 설정

--- app/src/main/java/jeonghwan/app/gpa/ui/screen/navi/map/GpWindow.kt
+++ app/src/main/java/jeonghwan/app/gpa/ui/screen/navi/map/GpWindow.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -82,6 +83,7 @@ fun GpWindowBox(p: PersonEntity) {
     Box(
         modifier =
             Modifier
+                .width(200.dp)
                 .background(Color.White, shape = RoundedCornerShape(16.dp))
                 .padding(16.dp),
     ) {
