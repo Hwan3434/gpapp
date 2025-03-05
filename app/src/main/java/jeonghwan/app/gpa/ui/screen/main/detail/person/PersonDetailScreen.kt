713f5f709a2aec6e23dd05186c328b16053f672e
jeonghwan <cookiegon3434@gmail.com>
2025-03-05 21:16:31 +0900

    테스트 중...

--- app/src/main/java/jeonghwan/app/gpa/ui/screen/main/detail/person/PersonDetailScreen.kt
+++ app/src/main/java/jeonghwan/app/gpa/ui/screen/main/detail/person/PersonDetailScreen.kt
@@ -1,15 +1,21 @@
 package jeonghwan.app.gpa.ui.screen.main.detail.person
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import timber.log.Timber
 
 @Composable
 fun PersonDetailScreen(
@@ -25,21 +31,14 @@ fun PersonDetailScreen(
         viewModel.fetchPersonDetail(personKey)
     }
 
-//    if(uiState.isLoading){
-//        Scaffold(
-//            modifier = modifier,
-//        ) { innerPadding ->
-//            Box(
-//                modifier = Modifier
-//                    .padding(innerPadding)
-//                    .fillMaxSize(), // Box의 크기를 최대로 확장
-//                contentAlignment = Alignment.Center // 내부 콘텐츠를 가운데 정렬
-//            ){
-//                CircularProgressIndicator()
-//            }
-//        }
-//        return
-//    }
+
+    val loading by remember {
+        derivedStateOf { uiState.isLoading }
+    }
+
+    val person by remember {
+        derivedStateOf { uiState.personEntity }
+    }
 
     Scaffold(
         modifier = modifier,
@@ -54,27 +53,40 @@ fun PersonDetailScreen(
                 )
                 FavoriteIcon(
                     person = personEntity,
-                    isFavorite = uiState.isFavorite,
+                    isFavorite = loading,
                     onFavoriteCheckedChange = viewModel::toggleFavorite,
                 )
             }
         },
     ) { innerPadding ->
-        Box(
-            modifier = Modifier.padding(innerPadding),
-        ) {
-            PersonDetailWidget(
-                person = uiState.personEntity,
-                father = uiState.father,
-//                mother = uiState.mother,
-//                spouse = uiState.spouse,
-//                children = uiState.children.toImmutableList(),
-                goToPerson = {
-                    viewModel.fetchPersonDetail(it)
-                },
-            )
+        if (loading || person == null) {
+            Box(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(), // Box의 크기를 최대로 확장
+                contentAlignment = Alignment.Center // 내부 콘텐츠를 가운데 정렬
+            ) {
+                CircularProgressIndicator()
+            }
+        } else {
+            Box(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(), // Box의 크기를 최대로 확장,
+            ) {
+                PersonDetailWidget(
+                    person = uiState.personEntity!!,
+                    father = uiState.father,
+                    mother = uiState.mother,
+                    spouse = uiState.spouse,
+                    children = uiState.children,
+                    goToPerson = viewModel::fetchPersonDetail,
+                )
+            }
         }
+
     }
+
 }
 
 @Preview
